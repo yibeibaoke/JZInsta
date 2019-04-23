b0bb0548d26ca3ba31361351dd848dab97c908da
WenTailai <wentailai.jiuzhang@gmail.com>
2019-04-23 17:48:51 -0400

    Create insta app

--- JZInsta/settings.py
+++ JZInsta/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'insta',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'JZInsta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- insta/urls.py
+++ insta/urls.py
@@ -1,4 +1,4 @@
-"""JZInsta URL Configuration
+"""insta app url Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+
+from insta.views import IndexView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('insta.urls')),
+    path('', IndexView.as_view(), name='index'),
 ]
